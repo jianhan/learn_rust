d7a2882b445c74709e275639b2b6c7b3107184cf
Jian Han <jianhan@mail.com>
2019-03-15 00:20:52 +1000

    more example

--- ch04/string_ownership/src/main.rs
+++ ch04/string_ownership/src/main.rs
@@ -3,18 +3,46 @@ fn main() {
 
     takes_ownership(s); // s's value moves into the function...
                         // ... and so is no longer valid here
-
+                        // println!("{}",s);
     let x = 5; // x comes into scope
 
     makes_copy(x);
+    println!("{}", x);
+    let s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
+
+    let s2 = String::from("hello");     // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2);  // s2 is moved into
 }
 
-
-fn takes_ownership(some_string: String) { // some_string comes into scope
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
     println!("{}", some_string);
 } // Here, some_string goes out of scope and `drop` is called. The backing
   // memory is freed.
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
     println!("{}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+fn gives_ownership() -> String {
+    // gives_ownership will move its
+    // return value into the function
+    // that calls it
+
+    let some_string = String::from("hello"); // some_string comes into scope
+
+    some_string // some_string is returned and
+                // moves out to the calling
+                // function
+}
+
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into
+    // scope
+
+    a_string // a_string is returned and moves out to the calling function
+}
