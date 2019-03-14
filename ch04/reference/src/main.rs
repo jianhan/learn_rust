cb096d71eaa361a04625f3c2adf9a383cb05b352
Jian Han <jianhan@mail.com>
2019-03-15 00:36:53 +1000

    add mutable example for string

--- ch04/reference/src/main.rs
+++ ch04/reference/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
-    let str = String::from("test");
-    println!("Hello, world! {}", count_str(str));
+    let str_test = String::from("test");
+    println!("Hello, world! {}", count_str(str_test));
     // println!("str is {}", str); -- this will trigger error error: Could not compile `reference
 
     let str2 = String::from("test2");
     let size = count_str_ref(&str2);
     println!("{} size is {}", str2, size);
-    
+
+    let str3 = String::from("test3");
+    let str3_result = double_str(str3);
+    println!("doulbe str is {}", str3_result);
 }
 
 fn count_str(str: String) -> usize {
@@ -15,4 +18,9 @@ fn count_str(str: String) -> usize {
 
 fn count_str_ref(str: &String) -> usize {
     return str.len();
+}
+
+fn double_str(mut str_test: String) -> String {
+    str_test.push_str(&str_test.to_string());
+    str_test
 }
