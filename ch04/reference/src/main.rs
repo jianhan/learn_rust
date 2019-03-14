e00f3cf1212d999fff335354acd612659281af2b
Jian Han <jianhan@mail.com>
2019-03-15 00:43:13 +1000

    dangle example

--- ch04/reference/src/main.rs
+++ ch04/reference/src/main.rs
@@ -10,6 +10,8 @@ fn main() {
     let str3 = String::from("test3");
     let str3_result = double_str(str3);
     println!("doulbe str is {}", str3_result);
+
+    let str4 = dangle();
 }
 
 fn count_str(str: String) -> usize {
@@ -24,3 +26,12 @@ fn double_str(mut str_test: String) -> String {
     str_test.push_str(&str_test.to_string());
     str_test
 }
+
+
+fn dangle() -> &String { // dangle returns a reference to a String
+
+    let s = String::from("hello"); // s is a new String
+
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
