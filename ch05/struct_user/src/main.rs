cae19c8e0f97ca83947b17b1a21c1deed409020b
Jian Han <jianhan@mail.com>
2019-03-15 14:14:12 +1000

    add address to user

--- ch05/struct_user/src/main.rs
+++ ch05/struct_user/src/main.rs
@@ -1,9 +1,19 @@
 #[derive(Debug)]
+struct Address {
+    street_number: u64,
+    street_name: String,
+    suburb: String,
+    state: String,
+    country: String,
+}
+
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
+    address: Address,
 }
 
 #[derive(Debug)]
@@ -13,17 +23,28 @@ struct Rectangle {
 }
 
 fn main() {
+    let a = Address {
+        street_number: 12,
+        street_name: String::from("smith street"),
+        suburb: String::from("King"),
+        state: String::from("QLD"),
+        country: String::from("Australia"),
+    };
+
     let user = User {
         username: String::from("johnsmith"),
         email: String::from("johnsmith@gmail.com"),
         sign_in_count: 12,
         active: false,
+        address: a,
     };
+
     let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
+        address: a,
     };
 
     user1.email = String::from("anotheremail@example.com");
@@ -31,6 +52,7 @@ fn main() {
     let user3 = build_user(
         String::from("someone@example.com"),
         String::from("test1@gmail.com"),
+        a,
     );
 
     let r = Rectangle {
@@ -40,10 +62,11 @@ fn main() {
     println!("{} {:?} {:#?}", area(&r), r, r);
 }
 
-fn build_user(username: String, email: String) -> User {
+fn build_user(username: String, email: String, address: Address) -> User {
     User {
         username,
         email,
+        address,
         sign_in_count: 0,
         active: false,
     }
