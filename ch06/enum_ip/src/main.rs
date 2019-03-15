5db2c999d4cff88701363b3c6564505cc0d0d360
Jian Han <jianhan@mail.com>
2019-03-15 17:21:34 +1000

    coin enums

--- ch06/enum_ip/src/main.rs
+++ ch06/enum_ip/src/main.rs
@@ -26,6 +26,22 @@ enum Option<T> {
     None,
 }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
     let four = IpAddress {
         kind: IpAddrKind::V4,
