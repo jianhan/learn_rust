3add626bed0e0c9fe33a78c20fd2e73a1c7226fa
Jian Han <jianhan@mail.com>
2019-03-15 13:48:46 +1000

    add build user fn

--- ch05/struct_user/src/main.rs
+++ ch05/struct_user/src/main.rs
@@ -12,5 +12,23 @@ fn main() {
         sign_in_count: 12,
         active: false,
     };
-    println!("Hello, world!");
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user3 = build_user(String::from("someone@example.com"), String::from("test1@gmail.com"));
+}
+
+fn build_user(username: String, email: String) -> User {
+    User {
+        username,
+        email,
+        sign_in_count: 0,
+        active: false,
+    }
 }
