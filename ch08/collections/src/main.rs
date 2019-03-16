84a52e4079c40b4914096681ddfe585a68c63b0f
Jian Han <jianhan@mail.com>
2019-03-16 17:10:32 +1000

    vector different types

--- ch08/collections/src/main.rs
+++ ch08/collections/src/main.rs
@@ -10,4 +10,32 @@ fn main() {
     mv.push("test5");
 
     println!("mv {:#?}", mv);
+
+    // get element out
+    let e = mv[0];
+    println!("index {:#?}", e);
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        // To change the value that the mutable reference refers to, we have to use the dereference
+        // operator (*) to get to the value in i before we can use the += operator
+        *i += 50;
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
