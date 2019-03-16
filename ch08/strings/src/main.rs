d31bfd5396f4a5c37329a42e5fb6070c89d3fc6d
Jian Han <jianhan@mail.com>
2019-03-16 21:08:53 +1000

    add two string

--- ch08/strings/src/main.rs
+++ ch08/strings/src/main.rs
@@ -11,4 +11,7 @@ fn main() {
     // the method also works on a literal directly:
     let s = "initial contents".to_string();
 
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
 }
